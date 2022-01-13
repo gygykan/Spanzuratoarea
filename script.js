40554aebb1ae790d7074ff2d691e1ba6ce886a53
SabinaMstan <93779021+SabinaMstan@users.noreply.github.com>
2022-01-13 15:30:37 +0200

    Cu animatie
    Spanzuratoare cu animatie

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ let wordToGuess = pickWord();
 let countLetters = 0;
 let lives = 10;
 let textToGuess = [];
+let indexFrame = 0;
 
 function startGame() {
   document.getElementById('start').disabled = true;
   document.getElementById('hide').style.display = 'block';
+  document.getElementById('hangman').src = './images/0.png';
   let word = document.getElementById('word');
   for (let letterIndex = 0; letterIndex < wordToGuess.length; ++letterIndex) {
     textToGuess[letterIndex] = document.createElement('input');
@@ -20,10 +22,15 @@ function startGame() {
 
 function pickWord() {
   const wordList = ['marble','abracadabra','supercalifragilistic','outerworldly','cacamaca'];
-  randomNumber = Math.floor(Math.random() * wordList.length);
+  randomNumber = getRandomNumber(wordList);
   let wordToGuess = wordList[randomNumber];
   return wordToGuess;
 }
+
+function getRandomNumber(wordList) {
+  return Math.floor(Math.random() * wordList.length);
+}
+
 
 function setLife() {
   let life = document.getElementById('life');
@@ -35,7 +42,10 @@ function setLife() {
 }
 
 function addKeyboard() {
-  const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',' q','r','s','t','u','v','w','x','y','z'];
+  let alphabet = [];
+  for(i = 10; i < 36; ++i){
+    alphabet += i.toString(36);
+  }
   let keyboard = document.getElementById('keyboard');
   for (let buttonIndex = 0; buttonIndex < alphabet.length; ++buttonIndex) {
       let button = document.createElement('button');
@@ -64,8 +74,29 @@ function validateLetter(letter) {
       }
     } else {
       --lives;
+      ++indexFrame;
+      animation();
       setLife();
     }
+}
+
+function animation() {
+  let imgPositions = [[10,0],[24,60],[24,60],[24,132],[46,121],[70,132],[73,113],[73,133],[105,133],[105,109],[50,125]];
+  let frame = [];
+  for (let indexImg = 1; indexImg <= imgPositions[0][0]; ++indexImg) {
+    frame[indexImg] = new Image();
+    frame[indexImg].src = `./images/${indexImg}.png`;
+    if (indexImg === indexFrame) {
+      document.getElementById('frames').appendChild(frame[indexImg]);
+    }
+  }
+  placeIt(frame[indexFrame], imgPositions[indexFrame][0], imgPositions[indexFrame][1]);
+}
+
+function placeIt(frame, x, y) {
+  frame.className = 'frame';
+  frame.style.top = x + 'px';
+  frame.style.left = y + 'px';
 }
 
 function getHint() {
